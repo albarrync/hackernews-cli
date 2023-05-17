39af7f122e53b5bc2bfb0841cde21fcd984de1c2
alexb <albarrync@gmail.com>
2023-05-16 22:23:27 -0400

    Scaffold Input

--- source/app.js
+++ source/app.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import { Text } from 'ink';
+import { Text, Box, useApp, useInput } from 'ink';
 import Spinner from 'ink-spinner';
 
 export default function App() {
 	// State
 	const [isLoaded, setIsLoaded] = React.useState(false);
 	const [stories, setStories] = React.useState([]);
-	const page_size = 15;
+	const [focusedStory, setFocusedStory] = React.useState(0);
+	const [page, setPage] = React.useState(0);
+
+	// Constants
+	const page_size = 20;
 	const hn_url = 'https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty';
+	const orange_color = '#ff9900';
 
 	// Init
 	React.useEffect(() => {
@@ -54,6 +59,40 @@ export default function App() {
 		}
 	}
 
+	// Input
+	const UserInput = () => {
+		const { exit } = useApp();
+
+		useInput((input, key) => {
+			if (input === 'q') {
+				exit();
+			}
+
+			if (input === 'r') {
+				loadStories(page_size);
+			}
+
+			if (input === 'n') {
+				// Get next page
+			}
+
+			if (input === 'p') {
+				// Get previous page
+			}
+
+			if (input === 'j') {
+				// Focus next story
+			}
+
+			if (input === 'k') {
+				// Focus previous story
+			}
+		});
+
+		return true
+	};
+
+
 
 	// Components
 	function HnLink(props) {
@@ -61,7 +100,7 @@ export default function App() {
 			<>
 				<Text>
 					<Text>{ordinalFormat(props.ordinal)}. </Text>
-					<Text color="#ff9900">{props.title} - {props.score} {props.score > 1 ? "points" : "point"}</Text>
+					<Text color={orange_color}>{props.title} - {props.score} {props.score > 1 ? "points" : "point"}</Text>
 				</Text>
 				<Text>
 					<Text>     </Text>
@@ -79,6 +118,13 @@ export default function App() {
 		)
 	}
 
+	function AppButton(props) {
+		return (
+			<Box borderStyle="round" borderColor={orange_color}>
+				<Text>{props.text}</Text>
+			</Box>
+		)};
+
 	// Render
 	return (
 		<>
@@ -88,6 +134,12 @@ export default function App() {
 																							title={story.title}
 																							score={story.score}
 																							url={story.url} /> )) }
+			<Box>
+				<AppButton text="Next"/>
+				<AppButton text="Last"/>
+				<AppButton text="Refresh"/>
+			</Box>
+			{ UserInput() }
 		</>
 	);
 }
